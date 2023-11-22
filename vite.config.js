5d4892cb4b9570e4cf217d6713516f930a2c339c
Vikesh8107 <anonymsdc123@gmail.com>
2023-11-22 18:32:06 +0530

    front end deploying in vercel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Add your build configuration here
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 })
